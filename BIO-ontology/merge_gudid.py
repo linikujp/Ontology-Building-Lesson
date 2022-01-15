4e23236388a69cbb900aa238d87b00adc0098471
alimusab20 <97367899+alimusab20@users.noreply.github.com>
2022-01-14 21:41:49 -0500

    Add files via upload

---
+++ BIO-ontology/merge_gudid.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import xlsxwriter
+import os
+import tempfile
+import shutil
+from functools import reduce
+
+
+def make_sheets(gudid_folder):
+    tempfile.TMPDIR = os.getcwd()
+    tf = tempfile.NamedTemporaryFile(dir=tempfile.TMPDIR)
+
+    files = ['device.txt',
+             'deviceSizes.txt',
+             'productCodes.txt',
+             'identifiers.txt',
+             'gmdnTerms.txt']
+    sheets = []
+    sheet_names = []
+    for f in files:
+        with open(f"{gudid_folder}/{f}") as file:
+            try:
+                sheet = pd.read_csv(file, sep="|", header=0)
+                sheets.append(sheet)
+                sheet_names.append(os.path.basename(file.name).replace('.txt',''))
+            except pd.errors.EmptyDataError as e:
+                print(f"make_sheets.py: {e}: {os.path.basename(file.name)}")
+    
+    writer = pd.ExcelWriter(tf, engine="xlsxwriter")
+    for s, sn in zip(sheets, sheet_names):
+        s.to_excel(writer, index=False, sheet_name=f"{sn}")
+    writer.save()
+
+    return tf
+
+def merge_sheets(file):
+    sheets_dict = pd.read_excel(file, sheet_name=None)
+
+    df = reduce(lambda left, right: pd.merge(left, right, on="PrimaryDI", how="outer"), sheets_dict.values())
+    return df
+
+
